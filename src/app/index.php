e23cccbba4b3d8d2f61fcfa15fa460ea62c390f2
hmpsEng <hampus.engblom@gmail.com>
2022-03-11 04:01:47 +0100

    update

---
+++ src/app/index.php
@@ -0,0 +1,8 @@
+<?php
+  error_reporting(E_ALL | E_STRICT); // enable for better debugging.
+
+  require_once './settings.php';
+
+  include_once './templates/header.php';
+  include_once './templates/footer.php';
+?>
---
+++ src/app/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+//single until shit work, multi later
+$language = 'en';
+
+//$website_url = 'https://no-site-url.com/';
+$website_url = '/'; // localhost Docker dev
+
+//note manifest.json settings.
+$webapp_name = 'in settings named';
+$brand_color = '#000';
+
+//settings for template/meta.php
+$website_meta = array();
+$website_meta['title'] = 'in settings title';
+$website_meta['keywords'] = 'in settings keyword';
+$website_meta['description'] = 'in settings descriton';
+$website_meta['robots'] = 'index,follow';
